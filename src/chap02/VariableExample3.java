303653d089ea47a7645631e735e8e3c5d8e26255
a29cute <allsun0829@gmail.com>
2024-08-30 15:42:14 +0900

    [정밀도 차이]

---
+++ src/chap02/VariableExample3.java
@@ -0,0 +1,18 @@
+package chap02;
+
+public class VariableExample3 {
+    public static void main(String[] args) {
+        // 실수값 저장
+        double var1 = 3.14;
+        float var2 = 3.14F;
+
+        // 정밀도 테스트
+        double var3 = 0.1234567890123456789;
+        float var4 = 0.1234567890123456789F;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+    }
+}
