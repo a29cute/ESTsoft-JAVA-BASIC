2c2b5a87496bc83ba0a8ac133944bb59c30d902b
a29cute <allsun0829@gmail.com>
2024-09-02 10:47:21 +0900

    [강제 타입 변환 퀴즈]

---
+++ src/chap02/casting/CastingQuiz.java
@@ -0,0 +1,27 @@
+package chap02.casting;
+
+public class CastingQuiz {
+    public static void main(String[] args) {
+        int intValue = 10;
+        char charValue = 'A';
+        double doubleValue = 5.7;
+        String stringValue = "A";
+
+        // 강제타입 변환
+        double var1 = (double) intValue; // int를 double로 변환
+        byte var2 = (byte) intValue;     // int를 byte로 변환 (손실 가능성 있음)
+        int var3 = (int) doubleValue;    // double을 int로 변환 (소수점 이하 손실됨)
+
+        // 문자열 값은 Char 타입으로 변경이 불가능하다.
+        // char var4 = (char) stringValue;
+        // 가능한 방법으로는 charAt(index) 라는 메서드를 사용해야 한다.
+        char changedValue = stringValue.charAt(0); // 문자열의 첫 번째 문자 추출
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(changedValue);
+
+
+    }
+}
