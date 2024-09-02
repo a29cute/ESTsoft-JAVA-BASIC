5097a6d2ac3977e83b615f03203272f30a650438
a29cute <allsun0829@gmail.com>
2024-09-02 10:46:43 +0900

    boolean 타입 예제

---
+++ src/chap02/VariableExample4.java
@@ -0,0 +1,12 @@
+package chap02;
+
+public class VariableExample4 {
+    public static void main(String[] args) {
+        boolean stop = false;
+        if (stop) {
+            System.out.println("중지합니다!");
+        } else {
+            System.out.println("시작합니다!");
+        }
+    }
+}
