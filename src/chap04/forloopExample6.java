985e810f3c72b21aa2647eb33f61f30bce5cb6a1
a29cute <allsun0829@gmail.com>
2024-09-04 15:10:24 +0900

    [삼중 for문]

---
+++ src/chap04/forloopExample6.java
@@ -0,0 +1,13 @@
+package chap04;
+
+public class forloopExample6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                for (int k = 1; k <= 3; k++) {
+                    System.out.println("" + i + j + k);  // ijk 각각의 숫자 출력
+                }
+            }
+        }
+    }
+}
