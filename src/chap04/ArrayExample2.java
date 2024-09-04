c9bf38334f44209c7ab4320f48072487bcc9efb4
a29cute <allsun0829@gmail.com>
2024-09-04 14:05:36 +0900

    [for문 배열 출력]

---
+++ src/chap04/ArrayExample2.java
@@ -0,0 +1,9 @@
+package chap04;
+
+public class ArrayExample2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
