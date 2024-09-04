95280ebdbb70d4c90eae4c67d082abe48ec3033d
a29cute <allsun0829@gmail.com>
2024-09-04 15:49:11 +0900

    [do~while문 3의 배수 출력]

---
+++ src/chap04/DoWhileloop.java
@@ -0,0 +1,11 @@
+package chap04;
+
+public class DoWhileloop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            i+=3;
+            System.out.println(i+"는 3의 배수");
+        } while(i<60);
+    }
+}
