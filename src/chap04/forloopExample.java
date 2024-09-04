5ebffc0260ae7ea11dd9558c202f6f810ae4e4c1
a29cute <allsun0829@gmail.com>
2024-09-04 11:42:45 +0900

    [for문/1~50까지 짝수 출력]

---
+++ src/chap04/forloopExample.java
@@ -0,0 +1,11 @@
+package chap04;
+
+public class forloopExample {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 == 0) { // 짝수 출력
+                System.out.println(i);
+            }
+        }
+    }
+}
