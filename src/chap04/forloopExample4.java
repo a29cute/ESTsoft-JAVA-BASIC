b7b71af5d6fafdb335a19d7a4555e2d4161c71d7
a29cute <allsun0829@gmail.com>
2024-09-04 14:26:31 +0900

    [이중 for문]

--- src/chap04/forloopExample4.java
+++ src/chap04/forloopExample4.java
@@ -1,7 +1,6 @@
 package chap04;
 
 public class forloopExample4 {
-    /* continue 이해 */
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {  // i = 0, 1, 2
             for (int j = 0; j < 4; j++) {  // j = 0, 1, 2, 3
