ee39e9b8092a5794c26450e99b38bab172a5292d
a29cute <allsun0829@gmail.com>
2024-09-04 14:26:52 +0900

    [continue의 이해]

--- src/chap04/forloopExample3.java
+++ src/chap04/forloopExample3.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 public class forloopExample3 {
     /* continue 이해 */
     public static void main(String[] args) {
-        for (int i = 1; i <= 60; i++) {
-            if(i%3!=0){
-                continue;
-            }
+//        for (int i = 1; i <= 60; i++) {
+//            if(i%3!=0){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//
+        for (int i = 1; i <= 60; i+=2) {
             System.out.println(i);
         }
     }
