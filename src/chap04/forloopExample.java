c3d7525ff72e5628f0e438f7e251d0d8ca3b433b
a29cute <allsun0829@gmail.com>
2024-09-04 14:05:54 +0900

    [for문 짝수 출력]

--- src/chap04/forloopExample.java
+++ src/chap04/forloopExample.java
@@ -3,9 +3,13 @@ package chap04;
 public class forloopExample {
     public static void main(String[] args) {
         for (int i = 0; i <= 50; i++) {
-            if (i % 2 == 0) { // 짝수 출력
+            if (i % 2 == 0) { // if문으로 조건식 추가
                 System.out.println(i);
             }
+        }
+
+        for (int i = 0; i <= 50; i+=2) { // for문만 이용
+            System.out.println(i);
         }
     }
 }
