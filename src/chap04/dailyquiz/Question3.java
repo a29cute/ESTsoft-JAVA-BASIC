a097c9dfb6750384d0a0d0f16b0b240ad8292ddd
a29cute <allsun0829@gmail.com>
2024-09-05 09:11:22 +0900

    [짝수 출력]

---
+++ src/chap04/dailyquiz/Question3.java
@@ -0,0 +1,11 @@
+package chap04.dailyquiz;
+
+public class Question3 {
+    public static void main(String args[]) {
+        // 0부터 20까지 짝수만 출력하는 코드를 작성해보세요.
+
+        for (int i = 0; i <= 20; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
