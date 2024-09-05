bf0813f8b6f2149eafc6e88b05ebb8bd3f5245bc
a29cute <allsun0829@gmail.com>
2024-09-05 16:04:50 +0900

    [문자열 퀴즈]

--- src/chap05/StringQuestion.java
+++ src/chap05/StringQuestion.java
@@ -5,16 +5,15 @@ public class StringQuestion {
         /**
          * Q1. "aaaabbccd"문자열 한 글자씩 출력하기
          */
-        String hello = "aaaabbccd";
-        for (int i = 0; i < hello.length(); i++) {
-            System.out.println(hello.charAt(i));
+        String str1 = "aaaabbccd";
+        for (int i = 0; i < str1.length(); i++) {
+            System.out.println(str1.charAt(i));
         }
-
         /**
          * Q2. 단어 순서 뒤집기
          */
-        String str = "Hello Welcome Java";
-        String[] words = str.split(" ");
+        String str2 = "Hello Welcome Java";
+        String[] words = str2.split(" ");
         String temp = words[0];
         words[0] = words[2];
         words[2] = temp;
@@ -24,7 +23,27 @@ public class StringQuestion {
         /**
          * Q3. 공백 제외해서 하나의 문자로 출력
          */
-        String str2 = "   Hello Welcome Java   ";
-        System.out.println(str2.replace(" ", ""));
+        String str3 = "   Hello Welcome Java   ";
+        System.out.println(str3.replace(" ", ""));
+
+        /**
+         * Q4. 연속된 문자의 반복 횟수로 문자열 압축해보기
+         */
+        String str4 = "aaaabbccd";//String str4 = "a4b2c2d1";
+        StringBuilder builder = new StringBuilder();
+        int count = 0; // 연속된 문자 저장
+        char current = str4.charAt(0); // 현재 분석 중인 문자 저장
+
+        for (int i = 0; i < str4.length(); i++) {
+            if(current == str4.charAt(i)) { //현재 문자가 str의 i번째와 같다면?
+                count++; // 같은 문자의 갯수 카운트 증가
+            } else { // 현재문자가 str의 i와 다르면?
+              builder.append(current).append(count);
+              current = str4.charAt(i); // 새로운 문자로 업데이트
+                count = 1; // 새로운 문자의 개수 초기화
+            }
+        }
+        builder.append(current).append(count);
+        System.out.println(builder.toString());
     }
 }
