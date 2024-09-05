c9b07fcdab5a116dd67bcffca78b843d385af7ab
a29cute <allsun0829@gmail.com>
2024-09-05 11:51:49 +0900

    [NullPointException]

---
+++ src/chap05/StringNullPointerExample.java
@@ -0,0 +1,8 @@
+package chap05;
+
+public class StringNullPointerExample {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.equals("비교하려는 값"));
+    }
+}
