12fb4738aff13f0045e956cbab685c89487ab0ca
a29cute <allsun0829@gmail.com>
2024-09-06 15:47:48 +0900

    [static final 수식자]

---
+++ src/chap06/Earth.java
@@ -0,0 +1,15 @@
+package chap06;
+
+public class Earth {
+	static final double EARTH_RADIUS = 6400;
+	static final double EARTH_SURFACE_AREA;
+
+	static {
+		EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+	}
+
+    public static void main(String[] args) {
+    		System.out.println("지구의 반지름: " + Earth.EARTH_RADIUS + "km");
+    		System.out.println("지구의 표면적: " + Earth.EARTH_SURFACE_AREA + " km^2");
+    }
+}
