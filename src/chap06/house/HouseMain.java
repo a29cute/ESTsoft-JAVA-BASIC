2b83862d15774e77a23c0219e9d9d45a3f1bfe84
a29cute <allsun0829@gmail.com>
2024-09-06 15:48:10 +0900

    [접근제어자]

---
+++ src/chap06/house/HouseMain.java
@@ -0,0 +1,8 @@
+package chap06.house;
+
+public class HouseMain {
+    public static void main(String[] args) {
+        HouseKim kim = new HouseKim();
+//        kim.lastname;
+    }
+}
---
+++ src/chap06/house/HousePark.java
@@ -0,0 +1,10 @@
+package chap06.house;
+
+public class HousePark {
+    String lastname = "park";
+
+       public static void main(String[] args) {
+           HouseKim kim = new HouseKim();
+           System.out.println(kim.lastname);  // HouseKim 클래스의 lastname 변수를 사용할 수 있다.
+       }
+}
