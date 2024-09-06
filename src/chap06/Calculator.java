6e1717fc1f2523d68eaa18e3ccbb4a6a94800982
a29cute <allsun0829@gmail.com>
2024-09-06 13:08:08 +0900

    [return과 void 이해]

--- src/chap06/Calculator.java
+++ src/chap06/Calculator.java
@@ -2,15 +2,14 @@ package chap06;
 
 public class Calculator {
     int postfixOperator(int a) {
-        a++;
-        return a;
+        a++; // a의 값을 1 증가시킵니다.
+        return a; // 증가된 a의 값을 반환합니다.
     }
-
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int a = 1;
-        int result = calculator.postfixOperator(a);
-        System.out.println(result);
+        int result = calculator.postfixOperator(a); // 메소드 호출 결과를 result 변수에 저장합니다.
+        System.out.println(result); // 증가된 값인 2를 출력합니다.
     }
 }
