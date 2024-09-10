080a9a7fbf188517c2a7c6504cff85b9ca45addd
a29cute <allsun0829@gmail.com>
2024-09-10 22:24:58 +0900

    [weekly2 퀴즈]

--- src/weekquiz02/Product.java
+++ src/weekquiz02/Product.java
@@ -4,29 +4,22 @@ public class Product implements Promotion {
     String name;
     int price;
     int weight;
-    int discountPrice;
 
     public Product(String name, int price, int weight) {
         this.name = name;
         this.price = price;
         this.weight = weight;
-
-        initDiscountPrice();
-    }
-
-    private void initDiscountPrice() {
-        if (this.name.equals("grocery")) {
-            this.discountPrice = 2000;
-        } else if (this.name.equals("beauty")) {
-            this.discountPrice = 10000;
-        } else {
-            this.discountPrice = 0;
-        }
     }
 
     @Override
     public int getDiscountAmount() {
-        return discountPrice;
+        if (this.name.equals("grocery")) {
+            return 2000;
+        } else if (this.name.equals("beauty")) {
+            return 10000;
+        } else {
+            return 0;
+        }
     }
 
     @Override
