6a2a809c02878b8b1a964e7756e301c63096bfbb
a29cute <allsun0829@gmail.com>
2024-09-10 14:08:29 +0900

    [인터페이스 다형성]

---
+++ src/chap08/poly/ProfileMain.java
@@ -0,0 +1,8 @@
+package chap08.poly;
+
+public class ProfileMain {
+    public static void main(String[] args){
+        ProfileService profileService = new ProfileService();
+        profileService.saveProfile();
+    }
+}
--- src/chap08/poly/ProfileService.java
+++ src/chap08/poly/ProfileService.java
@@ -1,11 +1,11 @@
 package chap08.poly;
 
 public class ProfileService {
-    ProfileRepository repository;
 
+    ProfileRepository repository;
     public void saveProfile(){
         repository= new ProfileDBRepository();
-        // repository = new ProfileMemoryRepository(); 이 부분만 바꿀 수 있음
+     // repository = new ProfileMemoryRepository(); 이 부분만 바꿀 수 있음
         repository.save();
 
     }
